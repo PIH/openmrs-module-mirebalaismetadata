4e7611f2975625b7db118fbc98f67be8a23125db
mseaton <mseaton@pih.org>
2015-03-31 09:44:18 -0400

    Use constants for tag names where possible

--- api/src/main/java/org/openmrs/module/mirebalaismetadata/constants/LocationTags.java
+++ api/src/main/java/org/openmrs/module/mirebalaismetadata/constants/LocationTags.java
@@ -14,7 +14,9 @@
 
 package org.openmrs.module.mirebalaismetadata.constants;
 
+import org.openmrs.module.emrapi.EmrApiConstants;
 import org.openmrs.module.mirebalaismetadata.descriptor.LocationTagDescriptor;
+import org.openmrs.module.paperrecord.PaperRecordConstants;
 
 /**
  * Constants for all defined location tags
@@ -23,37 +25,37 @@ public class LocationTags {
 
 	public static LocationTagDescriptor MEDICAL_RECORD_LOCATION = new LocationTagDescriptor() {
 		public String uuid() { return "71c99f93-bc0c-4a44-b573-a7ac096ff636"; }
-		public String name() { return "Medical Record Location"; }
+		public String name() { return PaperRecordConstants.LOCATION_TAG_MEDICAL_RECORD_LOCATION; }
 		public String description() { return "Notes that this location is a valid identifier location for an identifier that references a paper medical record"; }
 	};
 
 	public static LocationTagDescriptor ARCHIVES_LOCATION = new LocationTagDescriptor() {
 		public String uuid() { return "fa2c2030-65fb-11e4-9803-0800200c9a66"; }
-		public String name() { return "Archives Location"; }
+		public String name() { return PaperRecordConstants.LOCATION_TAG_ARCHIVES_LOCATION; }
 		public String description() { return "A location that serves as an archives for storing medical records"; }
 	};
 
 	public static LocationTagDescriptor LOGIN_LOCATION = new LocationTagDescriptor() {
 		public String uuid() { return "b8bbf83e-645f-451f-8efe-a0db56f09676"; }
-		public String name() { return "Login Location"; }
+		public String name() { return EmrApiConstants.LOCATION_TAG_SUPPORTS_LOGIN; }
 		public String description() { return "When a user logs in and chooses a session location, they may only choose one with this tag"; }
 	};
 
 	public static LocationTagDescriptor VISIT_LOCATION = new LocationTagDescriptor() {
 		public String uuid() { return "0967d73d-f3c9-492b-abed-356478610a94"; }
-		public String name() { return "Visit Location"; }
+		public String name() { return EmrApiConstants.LOCATION_TAG_SUPPORTS_VISITS; }
 		public String description() { return "Visits are only allowed to happen at locations tagged with this location tag or at locations that descend from a location tagged with this tag."; }
 	};
 
 	public static LocationTagDescriptor ADMISSION_LOCATION = new LocationTagDescriptor() {
 		public String uuid() { return "f5b9737b-14d5-402b-8475-dd558808e172"; }
-		public String name() { return "Admission Location"; }
+		public String name() { return EmrApiConstants.LOCATION_TAG_SUPPORTS_ADMISSION; }
 		public String description() { return "Patients may only be admitted to inpatient care in a location with this tag"; }
 	};
 
 	public static LocationTagDescriptor TRANSFER_LOCAITON = new LocationTagDescriptor() {
 		public String uuid() { return "9783aba6-df7b-4969-be6e-1e03e7a08965"; }
-		public String name() { return "Transfer Location"; }
+		public String name() { return EmrApiConstants.LOCATION_TAG_SUPPORTS_TRANSFER; }
 		public String description() { return "Patients may only be transfer to inpatient care in a location with this tag"; }
 	};
 
@@ -77,7 +79,7 @@ public class LocationTags {
 
 	public static LocationTagDescriptor DISPENSING_LOCATION = new LocationTagDescriptor() {
 		public String uuid() { return "c42b7bc3-b34d-4b8f-9796-09208f9dfd72"; }
-		public String name() { return "Dispensing Location"; }
+		public String name() { return EmrApiConstants.LOCATION_TAG_SUPPORTS_DISPENSING; }
 		public String description() { return "A location where a pharmacist or pharmacist aide can dispensed medication."; }
 	};
 
