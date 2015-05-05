6e24cf2e0f5ef90f3ad862153be975c59f3e4eef
mogoodrich <mgoodrich@pih.org>
2015-05-05 16:06:25 -0400

    added HtmlFormEntryui as a dependency in the pom

--- api/src/main/java/org/openmrs/module/mirebalaismetadata/MirebalaisMetadataProperties.java
+++ api/src/main/java/org/openmrs/module/mirebalaismetadata/MirebalaisMetadataProperties.java
@@ -1,6 +1,5 @@
 package org.openmrs.module.mirebalaismetadata;
 
-import org.openmrs.EncounterType;
 import org.openmrs.module.emrapi.utils.ModuleProperties;
 import org.springframework.stereotype.Component;
 
@@ -13,9 +12,6 @@ public class MirebalaisMetadataProperties extends ModuleProperties {
     public static final String GP_INSTALLED_ADDRESS_HIERARCHY_VERSION = "mirebalais.installedAddressHierarchyVersion";
     public static final String GP_INSTALLED_DRUG_LIST_VERSION = "mirebalaismetadata.installedDrugListVersion";
 
-    // encounter types
-    public static final String DEATH_CERTIFICATE_ENCOUNTER_TYPE_UUID = "1545d7ff-60f1-485e-9c95-5740b8e6634b";
-
     public int getInstalledDrugListVersion() {
         return getIntegerByGlobalProperty(GP_INSTALLED_DRUG_LIST_VERSION);
     }
@@ -23,9 +19,4 @@ public class MirebalaisMetadataProperties extends ModuleProperties {
     public int getInstalledAddressHierarchyVersion() {
         return getIntegerByGlobalProperty(GP_INSTALLED_ADDRESS_HIERARCHY_VERSION);
     }
-
-    public EncounterType getDeathCertificateEncounterType() {
-        return getEncounterTypeByUuid(DEATH_CERTIFICATE_ENCOUNTER_TYPE_UUID, true);
-    }
-
 }
