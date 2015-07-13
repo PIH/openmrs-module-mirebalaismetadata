65cf4c51f17ccdf66e5db3fbdf6f01902f5158b1
lnball <eball@pih.org>
2015-07-13 16:27:50 -0400

    UHM-2039 Added 3 new medications: zinc sulfate, salbutamol, propylthiouracil

--- api/src/main/java/org/openmrs/module/mirebalaismetadata/MirebalaisMetadataActivator.java
+++ api/src/main/java/org/openmrs/module/mirebalaismetadata/MirebalaisMetadataActivator.java
@@ -55,7 +55,7 @@ public class MirebalaisMetadataActivator extends BaseModuleActivator {
 
     protected Log log = LogFactory.getLog(getClass());
 
-    protected static final Integer DRUG_LIST_VERSION = 5;
+    protected static final Integer DRUG_LIST_VERSION = 6;
 
     private MirebalaisMetadataProperties mirebalaisMetadataProperties;
 
