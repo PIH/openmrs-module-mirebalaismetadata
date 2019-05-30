cf491c37bc42fa4d223ad7cca0b03bcfe88599aa
Brandon Istenes <bistenes@gmail.com>
2019-05-29 22:52:36 -0500

    Upgrade a bunch of packages

--- mds-search/find-concept.sh
+++ mds-search/find-concept.sh
@@ -18,6 +18,6 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
-grep -l "<conceptId>$1</conceptId>" * | sed 's/metadata.xml~/header.xml~/' | xargs grep "<name>"
+grep -l "<conceptId>$1</conceptId>" * | sed 's/metadata.xml/header.xml/' | xargs grep "<name>"
 grep "<conceptId>$1</conceptId>" *
 grep -A50 "<conceptId>$1</conceptId>" * | grep -m 1 -A8 "<conceptNameId>" | grep "<name>"
